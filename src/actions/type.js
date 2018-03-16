94aec501936f81f6346f3c0e499c1acdfefde2e3
Andre Loureiro <loureiro.andrew@gmail.com>
2018-03-16 18:06:29 -0400

    feature: Add base app's action types
    Signed-off-by: Andre Loureiro <loureiro.andrew@gmail.com>

---
+++ src/actions/type.js
@@ -0,0 +1,11 @@
+export const GET_ALL_POSTS = 'GET_ALL_POSTS';
+export const ADD_POST = 'ADD_POST';
+export const EDIT_POST = 'EDIT_POST';
+export const REMOVE_POST = 'REMOVE_POST';
+export const VOTE_ON_POST = 'VOTE_ON_POST';
+
+export const GET_ALL_COMMENTS = 'GET_ALL_COMMENTS';
+export const ADD_COMMENT = 'ADD_COMMENT';
+export const REMOVE_COMMENT = 'REMOVE_COMMENT';
+export const EDIT_COMMENT = 'EDIT_COMMENT';
+export const VOTE_ON_COMMENT = 'VOTE_ON_COMMENT';
