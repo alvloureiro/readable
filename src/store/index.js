7f6cac7127b7e59c72f5cd281a2597563115f6d2
Andre Loureiro <loureiro.andrew@gmail.com>
2018-04-13 15:15:02 -0400

    refactor: Use saga middleware on store implementation
    We use redux-saga instead of redux-thunk to control the async states and
    start all sagas after we properly create the store.
    
    Signed-off-by: Andre Loureiro <loureiro.andrew@gmail.com>

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,14 @@
 import { createStore, applyMiddleware, compose } from 'redux';
-import thunk from 'redux-thunk';
+import createSagaMiddleware from 'redux-saga';
 import reducers from 'reducers';
+import sagas from 'sagas';
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION__ || compose;
+const reduxDevTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
 
-const store = createStore(reducers, composeEnhancers(applyMiddleware(thunk)));
+const sagaMiddleware = createSagaMiddleware();
+
+const store = createStore(reducers, compose(applyMiddleware(sagaMiddleware), reduxDevTools));
+
+sagaMiddleware.run(sagas);
 
 export default store;
