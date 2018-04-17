6621305f2abab100c3cca58476c19334ec8c545b
Andre Loureiro <loureiro.andrew@gmail.com>
2018-04-17 17:35:46 -0400

    feat: Add Readable base component
    Add `Readable` base component to wrap the application routes and some
    common ui components as the `AppHeader`, `Progress` and a default
    `SnackBar` to designate that an error has been happened.
    
    Signed-off-by: Andre Loureiro <loureiro.andrew@gmail.com>

---
+++ src/components/Readable.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Route } from 'react-router-dom';
+import Snackbar from 'material-ui/Snackbar';
+import AppHeader from './common/AppHeader';
+import PostList from './common/PostList';
+import Progress from './common/Progress';
+import { appFetchingInitialData } from 'actions';
+
+class Readable extends Component {
+  componentWillMount() {
+    this.props.appFetchingInitialData();
+  }
+
+  render() {
+    return (
+      <div>
+        {this.props.loading ? <Progress /> : null}
+        <AppHeader />
+        <Route exact path="/" render={() => <PostList />} />
+        <Snackbar
+          open={this.props.error ? true : false}
+          autoHideDuration={4000}
+          message="Error during the operation"
+        />
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = ({ app }) => {
+  const { loading } = app;
+  return { loading };
+};
+
+export default connect(mapStateToProps, {
+  appFetchingInitialData
+})(Readable);
