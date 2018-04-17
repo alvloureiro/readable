87f71f2cd463ff273e561fbd221031ae27508c39
Andre Loureiro <loureiro.andrew@gmail.com>
2018-04-17 17:40:41 -0400

    feat: Add `Progress` component
    Signed-off-by: Andre Loureiro <loureiro.andrew@gmail.com>

---
+++ src/components/common/Progress.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { pink300 } from 'material-ui/styles/colors';
+import LinearProgress from 'material-ui/LinearProgress';
+
+const Progress = () => <LinearProgress mode="indeterminate" color={pink300} />;
+
+export default Progress;
